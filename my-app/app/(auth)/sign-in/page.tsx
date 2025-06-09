1529ede718f5d55f7d08a066f4499f2cbad2e9f0
Merin Eldos <merineldos123@gmail.com>
2025-06-09 14:13:44 +0530

    changed sign-up and sign-in UI

---
+++ my-app/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Authform from "@/Components/Authform";
+
+const Page = () => {
+    return (
+        <div>
+            return <Authform type="sign-in"/>
+        </div>
+    );
+};
+
+export default Page;
---
+++ my-app/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Authform from "@/Components/Authform";
+
+const Page = () => {
+    return <Authform type="sign-up" />;
+};
+
+
+export default Page;
